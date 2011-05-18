d1e9c24f2120798760539e2d8daf760c2160d65c
Jeremy Jones <jeremy.jones@cazoodle.com>
2011-05-18 14:46:29 -0500

    initial code for scribe_cat

---
+++ scribe_cat.c
@@ -0,0 +1,12 @@
+#include <transport/thrift_transport.h>
+#include <transport/thrift_socket.h>
+#include <transport/thrift_server_transport.h>
+#include <transport/thrift_server_socket.h>
+#include <transport/thrift_framed_transport.h>
+
+int main(int argc, char **argv) {
+
+    
+
+
+}
